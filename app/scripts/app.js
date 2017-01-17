362285e038365228424dd32be1bb99bc80fa3a4e
Tara Briggs <tebriggs4@gmail.com>
2017-01-17 17:15:35 -0500

    correct ui-sref errors from previous assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,7 @@
         .state('album', {
             url: '/album',
             templateUrl: '/templates/album.html'
-        });
+        })
         .state('collection', {
             url: '/collection',
             templateUrl: '/templates/collection.html'
